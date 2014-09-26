21a08a1d4cb98a674957ebe9b88e337e028684c7
marnitto <marnitto@gmail.com>
2014-09-26 22:06:19 +0900

    Use unicode literal prefix when using interactive string

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ cmdclass = {}
 
 setup(
     name='Seektam-web',
-    description='스노인의 서비스 Seektam의 웹 프로그램',
+    description=u'스노인의 서비스 Seektam의 웹 프로그램',
     long_description=readme(),
     author='LeafDev',
     url='http://seektam.kr/',
