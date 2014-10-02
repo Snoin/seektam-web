82944119094fc959d3bec19a3f8c06361ca9267d
marnitto <marnitto@gmail.com>
2014-10-02 19:44:50 +0900

    Rename ambigious model name: food.py -> koreafood.py

--- tests/model/test_koreafood.py
+++ tests/model/test_koreafood.py
@@ -3,7 +3,7 @@
 import pytest
 from sqlalchemy import create_engine
 
-from seektam.model import food
+from seektam.model import koreafood
 from seektam.model import orm
 from tests.model.mock import MockDatabase
 
@@ -25,44 +25,46 @@ def _mockdb_add_model(mockdb, model, **kwarg):
     return a
 
 
-def test_food_model_based_on_orm():
-    assert food.Base == orm.Base
+def test_koreafood_model_based_on_orm():
+    assert koreafood.Base == orm.Base
 
 
-def test_food_aliment_relationship_1_1(mockdb):
+def test_koreafood_aliment_relationship_1_1(mockdb):
     # insert test data
     a = _mockdb_add_model(
-        mockdb, food.Aliment,
+        mockdb, koreafood.Aliment,
         id=1001, name='aliment_1')
 
     f = _mockdb_add_model(
-        mockdb, food.Food,
+        mockdb, koreafood.Food,
         id=2001, name='food_1', aliments=[a])
 
     # query
     newsess = mockdb._Session()
-    newf = newsess.query(food.Food).filter(food.Food.id == f.id).one()
+    newf = newsess.query(koreafood.Food).filter(
+        koreafood.Food.id == f.id).one()
 
     # validate
     assert len(newf.aliments) == 1
     assert newf.aliments[0].id == a.id
 
 
-def test_food_aliment_relationship_1_n(mockdb):
+def test_koreafood_aliment_relationship_1_n(mockdb):
     N = 3
     aliments = []
     for n in range(1000, 1000+N):
         a = _mockdb_add_model(
-            mockdb, food.Aliment,
+            mockdb, koreafood.Aliment,
             id=n, name='aliment_%s' % n)
         aliments.append(a)
 
     f = _mockdb_add_model(
-        mockdb, food.Food,
+        mockdb, koreafood.Food,
         id=2001, name='food_1', aliments=aliments)
 
     newsess = mockdb._Session()
-    newf = newsess.query(food.Food).filter(food.Food.id == f.id).one()
+    newf = newsess.query(koreafood.Food).filter(
+        koreafood.Food.id == f.id).one()
 
     assert len(newf.aliments) == N
     for n in range(N):
